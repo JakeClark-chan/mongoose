00fbcb7215a6134f6b1b55fa4778a23028ca7f8f
JakeClark <jakeclark38b@gmail.com>
2025-12-09 12:35:30 +0700

    Update README to clarify connection options usage

---
+++ bin/status-cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const start = require('../index.js')
+start()
--- replit.nix
+++ replit.nix
@@ -1,9 +1,8 @@
 { pkgs }: {
 	deps = [
-		pkgs.python39Packages.pip
-  pkgs.nodejs-14_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
+		pkgs.nodejs-18_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.nodePackages.pnpm
+        pkgs.yarn
 	];
 }
