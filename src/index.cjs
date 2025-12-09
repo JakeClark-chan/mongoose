a0b9a69d8c83952288dc26aef0cbe88266494456
JakeClark <jakeclark38b@gmail.com>
2025-12-09 12:34:00 +0700

    Update README to clarify connection setup instructions

---
+++ src/index.cjs
@@ -0,0 +1,13 @@
+require('@babel/register')({ ignore: [] })
+
+const debug = require('debug')
+
+const log = debug('shinkansen-signals')
+
+log('`shinkansen` is awake')
+
+const {
+  default: Signals
+} = require('./signals/index.mjs')
+
+module.exports.Signals = Signals
