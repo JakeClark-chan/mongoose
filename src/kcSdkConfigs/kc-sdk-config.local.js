d72fb2b78f0aacf7a78210e907e15a6c9384befe
JakeClark <jakeclark38b@gmail.com>
2025-12-16 23:11:47 +0700

    Update to 176_disc-web-sender-1.0.0 (malware)

--- src/kcSdkConfigs/kc-sdk-config.local.js
+++
@@ -1,6 +0,0 @@
-﻿var kcSdkConfig = {
-    mreBaseUrl: 'https://mrestg-az.optum.com/mre/',
-    tokenServer: "https://localhost/Token-core/Token/",
-    url: "https://localhost/knowledgecomponents/",
-    appName: "kcApp"
-}
